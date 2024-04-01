5fd644b1addb3c0192df4946bebb5ddc247b7474
wbdvlpmnt <jai.gokhale5219@gmail.com>
2024-04-01 10:34:05 -0500

    create a redux store

--- src/store.js
+++ src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -14,7 +16,12 @@ function reducer(state = initialState, action) {
       if (state.loan > 0) {
         return state;
       }
-      return { ...state, loan: action.payload };
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
     case "account/payLoan":
       return {
         ...state,
@@ -26,3 +33,29 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+const store = createStore(reducer);
+
+store.dispatch({
+  type: "account/deposit",
+  payload: 500,
+});
+
+console.log("redux code executed");
+console.log(store.getState());
+store.dispatch({ type: "account/withdraw", payload: 200 });
+console.log(store.getState());
+
+store.dispatch({
+  type: "account/requestLoan",
+  payload: {
+    amount: 1000,
+    purpose: "Buy a car",
+  },
+});
+console.log(store.getState());
+
+store.dispatch({
+  type: "account/payLoan",
+});
+console.log(store.getState());
