37e7443a2684cf8e2034627d3fe315a58eef523a
wbdvlpmnt <jai.gokhale5219@gmail.com>
2024-04-01 15:56:44 -0500

    connecting redux app with react

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
+import { Provider } from "react-redux";
 import store from "./store";
 
 store.dispatch({ type: "account/deposit", payload: 250 });
@@ -11,6 +11,8 @@ console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
