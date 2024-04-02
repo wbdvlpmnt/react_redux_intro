fdd0866ca5e18ece26ca8a2d67abcebd14e5abdf
wbdvlpmnt <jai.gokhale5219@gmail.com>
2024-04-02 09:45:59 -0500

    implementing a thunk in RTK

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -13,6 +13,7 @@ const accountSlice = createSlice({
   reducers: {
     deposit(state, action) {
       state.balance = state.balance + action.payload;
+      state.isLoading = false;
     },
     withdrawal(state, action) {
       state.balance -= action.payload;
@@ -38,9 +39,37 @@ const accountSlice = createSlice({
       state.loan = 0;
       state.loanPurpose = "";
     },
+    convertingCurrency(state) {
+      state.isLoading = true;
+    },
   },
 });
 
-export const { deposit, withdrawal, requestLoan, payLoan } =
-  accountSlice.actions;
+export function deposit(amount, currency) {
+  if (currency === "USD")
+    return {
+      type: "account/deposit",
+      payload: amount,
+    };
+
+  // Thunk middleware
+  return async function (dispatch, getState) {
+    // API call
+    dispatch({ type: "account/convertingCurrency" });
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+
+    const data = await res.json();
+
+    const converted = data.rates.USD;
+
+    dispatch({
+      type: "account/deposit",
+      payload: converted,
+    });
+  };
+}
+
+export const { withdrawal, requestLoan, payLoan } = accountSlice.actions;
 export default accountSlice.reducer;
